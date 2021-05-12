8e54d6d624b247be36b04c89248160afbb7f0888
govicse17 <govicse17@gmail.com>
2021-05-12 21:31:56 +0530

    list max

--- demo.py
+++ demo.py
@@ -1,3 +1,13 @@
-print ("Welcome")
-if (10>20):
-  print (" The number is lesser")
+A=[] # empty list
+size=int(input('Just pass the size of list'))
+for i in range(size):
+    x=int(input('pass an integer value into the list\n'))
+    A.append(x)
+print('The list value :',A)
+max=A[0]
+for i in A:
+    if i>max:
+        max=i
+print('Max value in the list:',max)
+
+
